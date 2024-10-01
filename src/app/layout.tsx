2876fb01a7d0d4df11ac62c8744c16dc0f14c39f
Guy Brown <guy.brown@justchecking.co.uk>
2024-10-01 21:14:40 +0100

    comit it eh

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import '@/styles/tailwind.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Consult Net Zero',
+    default: 'Consult Net Zero - Retrofitting Made Simple',
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://api.fontshare.com/css?f%5B%5D=switzer@400,500,600,700&amp;display=swap"
+        />
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="The Consult Net Zero Blog"
+          href="/blog/feed.xml"
+        />
+      </head>
+      <body className="text-gray-950 antialiased">{children}</body>
+    </html>
+  )
+}
